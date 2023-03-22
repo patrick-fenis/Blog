1fb7e268be3cb3988a9ef07b688596811ea7690e
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-22 18:48:03 -0400

    Moved frontend code to its own repo

--- frontend/src/components/Main.js
+++
@@ -1,6 +0,0 @@
-// main page boilerplate
-const Main = (props) => {
-  return (<h1>Main Page</h1>)
-}
-
-export default Main;
--- frontend/src/pages/Index.js
+++
@@ -1,5 +0,0 @@
-const Index = (props) => {
-  return (<h1>Index Page</h1>)
-}
-
-export default Index;
