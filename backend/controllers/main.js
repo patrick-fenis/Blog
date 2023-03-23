7981eb5b4bf02332d8c3dac0b5beec7fb14cbba4
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-03-22 22:06:59 -0400

    Post route created - Ready for first deployment

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -11,33 +11,40 @@ const BlogSchema = require('../models/blog')
 /// Routes
 ////////////////////////////////////////
 
-router.get('/seed', async (req, res) => {
-    const blogPosts = [
-        {
-            title: 'Test',
-            description: 'Test line of text',
-            image: 'URL Goes here'
-        },
-        {
-          title: 'Test2',
-          description: 'Test line of text number 3',
-          image: 'URL Goes here 2'
-        }
-    ]
+// router.get('/seed', async (req, res) => {
+//     const blogPosts = [
+//         {
+//             title: 'Test',
+//             description: 'Test line of text',
+//             image: 'URL Goes here'
+//         },
+//         {
+//           title: 'Test2',
+//           description: 'Test line of text number 3',
+//           image: 'URL Goes here 2'
+//         }
+//     ]
   
-    try {
-      const seedItems = await BlogSchema.create(blogPosts)
-      res.send(seedItems)
-    } catch (err) {
-      res.send(err.message)
-    }
-  })
+//     try {
+//       const seedItems = await BlogSchema.create(blogPosts)
+//       res.send(seedItems)
+//     } catch (err) {
+//       res.send(err.message)
+//     }
+//   })
 
 
 router.get("/", (req, res) => {
     res.send("Hello, World!")
   })
 
+//   router.post("/post", async (req, res) => {
+//     try {
+//         res.json(await BlogSchema.create(req.body));
+//     } catch (error) {
+//         res.status(400).json(error);
+//       }
+//   })
 
   // GET - BLOG INDEX
 // router.get("/blogs", (req, res) => {
