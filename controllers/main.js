37602a8333b1d9ea7fb1cf7022d63eba2107794a
Sergio <smontufar250@gmail.com>
2023-03-24 22:34:45 -0400

    updated put route

--- controllers/main.js
+++ controllers/main.js
@@ -35,14 +35,14 @@ const Blog = require('../models/blog')
 //     }
 //   })
 
-// Route to Test DB
-router.get("/test", async (req, res) => {
-  try {
-    res.json(await Blog.find({}))
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+// // Route to Test DB
+// router.get("/test", async (req, res) => {
+//   try {
+//     res.json(await Blog.find({}))
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
 
 // GET ROUTE - BLOG INDEX
 router.get("/", async (req, res) => {
@@ -53,16 +53,9 @@ router.get("/", async (req, res) => {
   }
 })
 
-// Edit Route - Edit BLOG
-router.put('/:id', (req, res) => {
-  const resourceId = req.params.id;
-  const updatedData = req.body;
-
-  res.json({ message: 'Resource updated successfully', data: updatedData });
-});
 
 // POST ROUTE - NEW BLOG
-router.post("/post", async (req, res) => {
+router.post("/new", async (req, res) => {
     try {
         res.json(await Blog.create(req.body));
     } catch (error) {
@@ -87,6 +80,19 @@ router.delete("/:id", async (req, res) => {
     res.status(400).json(error)
   }
 })
+
+// Put Route - Edit BLOG
+router.put('/:id', async (req, res) => {
+  const resourceId = req.params.id;
+  const updatedData = req.body;
+
+  try {
+    res.json(await Blog.findByIdAndUpdate(resourceId, updatedData, {new: true}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+  // res.json({ message: 'Resource updated successfully', data: updatedData });
+});
 
 //
 
